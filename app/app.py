75ebbcfcc807eadefa3ffbf047b6609f9b7dfbba
mimotej <mimotej@seznam.cz>
2020-10-24 14:06:13 +0200

    Basic report

--- app/app.py
+++ app/app.py
@@ -11,24 +11,24 @@ def user():
     return render_template('user.html')
 @app.route('/manage_users')
 def manage_users():
-    return render_template('manage_users.html')
+    return render_template('admin_only/manage_users.html')
 
 @app.route('/paid_action')
 def paid_action():
-    return render_template('paid_action.html')
+    return render_template('not_menu_accessible/paid_action.html')
 
 @app.route('/add_user')
 def add_user():
-    return render_template('add_user.html')
+    return render_template('admin_only/add_user.html')
 @app.route('/manage_paid_actions')
 def manage_paid_actions():
     return render_template('manage_paid_actions.html')
 @app.route('/report')
 def report():
-    return render_template('report.html')
-#@app.route('/tickets')
-#def tickets():
-#    return render_template('tickets.html')
+    return render_template('not_menu_accessible/report.html')
+@app.route('/ticket')
+def tickets():
+    return render_template('ticket.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
