80956e1d7ef10ba98289b2db52e7aba2f7bb0651
mimotej <mimotej@seznam.cz>
2020-10-26 12:01:32 +0100

    Base done

--- app/static/js/style-js.js
+++ app/static/js/style-js.js
@@ -14,11 +14,11 @@ manage_users.addEventListener("mouseout",function (){
         manage_users.classList.remove("hover");
     })
     */
-var allclasses=document.getElementsByClassName('hover-button');
+var allclasses = document.getElementsByClassName('hover-button');
 for (let i = 0; i < allclasses.length; i++) {
     allclasses[i].addEventListener("mouseover", function () {
-    allclasses[i].classList.add("hover");
-});
+        allclasses[i].classList.add("hover");
+    });
 }
 for (let i = 0; i < allclasses.length; i++) {
     allclasses[i].addEventListener("mouseout", function () {
@@ -28,19 +28,18 @@ for (let i = 0; i < allclasses.length; i++) {
 var modal_hide = document.getElementById('md01');
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (event.target == modal_hide) {
         modal_hide.style.display = "none";
     }
 }
-document.getElementById("pencil").addEventListener("click",function (){
-    var x= document.getElementsByClassName('edit-user-form-input');
+document.getElementById("pencil").addEventListener("click", function () {
+    var x = document.getElementsByClassName('edit-user-form-input');
     for (let i = 0; i < x.length; i++) {
-        if(x[i].style.display=='block'){
-            x[i].style.display='none';
-        }
-        else {
+        if (x[i].style.display == 'block') {
+            x[i].style.display = 'none';
+        } else {
             x[i].style.display = 'block';
         }
     }
-    })
+})
