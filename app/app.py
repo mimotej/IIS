a878a424df305edd2959fb788dbc80463402f72f
Michal Glos <mglos@redhat.com>
2020-10-29 14:13:49 +0100

    Add SQLAlchemy database definition, login, sign up

--- app/app.py
+++ app/app.py
@@ -1,22 +1,53 @@
-from flask import request
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_bcrypt import Bcrypt
 
-from flask import Flask, render_template
+from database import *
+from datetime import timedelta
+import logging as logger
+logger.basicConfig(level='DEBUG')
 
-app = Flask(__name__)
+from database import db, app
+bcrypt = Bcrypt(app)
 
+app.permanent_session_lifetime = timedelta(minutes=30)
 
 @app.route('/')
 def index():
+    import pdb; pdb.set_trace()
     return render_template('index.html')
 
-@app.route('/process_form', methods=['GET', 'POST'])
-def process_form():
-    if 'e-mail' in request.form:
-        return 'První formulář'
-    elif 'e-mail-reg' in request.form:
-        return 'Druhý formulář'
+@app.route('/logn', methods=['GET', 'POST'])
+def login():
+    '''Process data for login'''
+    if( request.method == "POST" ):
+        if( 'user_id' in session ):
+            logger.debug("You are logged in")
+        else:
+            user = User.query.filter_by(email=request.form['email']).first()
+            if user:
+                if bcrypt.check_password_hash(user.password, request.form['password']):
+                    session['user_id'] = user._id
+                    logger.debug("User logged in")
+                else:
+                    logger.debug("Bad password")
+            else:
+                logger.debug("User not found")
+    return redirect(url_for('index'))
+
+@app.route('/sign_up', methods=['GET', 'POST'])
+def sign_up():
+    '''Process data for sign up'''
+    data = request.form
+    import pdb; pdb.set_trace()
+    if(User.query.filter_by(email=data['email']).first()):
+        logger.debug("Email already registered")
     else:
-        return 'Chyba'
+        new_user = User(data['name'], data['surname'], bcrypt.generate_password_hash(data['password']), data['email'], data['phone'])
+        db.session.add(new_user)
+        db.session.commit()
+        logger.debug("New user registered")
+    return redirect(url_for('index'))
+
 @app.route('/user')
 def user():
     return render_template('user.html')
@@ -87,9 +118,11 @@ def medical_problem():
 @app.route('/medical_examinations')
 def medical_examinations():
     return render_template('doctor_only/medical_examinations.html')
+
 @app.route('/medical_examination')
 def medical_examination():
     return render_template('doctor_only/medical_examination.html')
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True, host='0.0.0.0')
